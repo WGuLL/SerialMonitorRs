a8a72af5a3008729674c1feff1950e1ad16b2baf
Fabien ROUSSEL <fabien.roussel.1992@gmail.com>
2022-09-24 00:04:02 +0200

    Build a simple window using fltk.

--- serial_term/src/main.rs
+++ serial_term/src/main.rs
@@ -1,23 +1,25 @@
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-
+use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
 fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            _ => (),
-        }
-    });
+    let app = app::App::default();
+    let mut wind = Window::default()
+        .with_size(160, 200)
+        .center_screen()
+        .with_label("Counter");
+    let mut frame = Frame::default()
+        .with_size(100, 40)
+        .center_of(&wind)
+        .with_label("0");
+    let mut _but_inc = Button::default()
+        .size_of(&frame)
+        .above_of(&frame, 0)
+        .with_label("+");
+    let mut _but_dec = Button::default()
+        .size_of(&frame)
+        .below_of(&frame, 0)
+        .with_label("-");
+    wind.make_resizable(true);
+    wind.end();
+    wind.show();
+    /* Event handling */
+    app.run().unwrap();
 }
-
