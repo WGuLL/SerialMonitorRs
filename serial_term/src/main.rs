01fc23b3269227c12d533fcf02df21da3de995ae
Fabien ROUSSEL <fabien.roussel.1992@gmail.com>
2022-09-25 00:29:16 +0200

    Add a callback to the choice menu.

--- serial_term/src/main.rs
+++ serial_term/src/main.rs
@@ -1,4 +1,11 @@
-use fltk::{app, menu::Choice, output::MultilineOutput, prelude::*, window::Window};
+use fltk::{
+    app,
+    enums::Shortcut,
+    menu::{Choice, MenuFlag},
+    output::MultilineOutput,
+    prelude::*,
+    window::Window,
+};
 use serial2::SerialPort;
 fn main() {
     let app = app::App::default();
@@ -17,7 +24,16 @@ fn main() {
     serial_port_list.unwrap().iter().for_each(move |item| {
         let choice_to_add = item.to_str();
         if choice_to_add.is_some() {
-            serial_port_selector.add_choice(choice_to_add.unwrap());
+            serial_port_selector.add(
+                choice_to_add.unwrap(),
+                Shortcut::None,
+                MenuFlag::Normal,
+                |choice| {
+                    let serial_port_to_open = choice.choice();
+                    assert!(serial_port_to_open.is_some());
+                    let _serial_port = SerialPort::open(serial_port_to_open.unwrap(), 115200);
+                },
+            );
         }
     });
 
