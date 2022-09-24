dba686e975191737f9cad41a941380281cf67740
Fabien ROUSSEL <fabien.roussel.1992@gmail.com>
2022-09-24 23:58:39 +0200

    Added the list of the available serial ports.

--- serial_term/src/main.rs
+++ serial_term/src/main.rs
@@ -1,18 +1,25 @@
-use fltk::{app, button::Button, output::MultilineOutput, prelude::*, window::Window};
+use fltk::{app, menu::Choice, output::MultilineOutput, prelude::*, window::Window};
+use serial2::SerialPort;
 fn main() {
     let app = app::App::default();
     let mut wind = Window::default()
         .with_size(400, 200)
         .center_screen()
         .with_label("SerialTerm");
-    let mut output_widget = MultilineOutput::default().with_size(wind.width(), 100);
-    let _test_button = Button::default()
+    let output_widget = MultilineOutput::default().with_size(wind.width(), 100);
+    let mut serial_port_selector = Choice::default()
         .with_size(100, 20)
-        .below_of(&output_widget, 32)
-        .with_label("test")
-        .set_callback(move |_| {
-            let _error_code = output_widget.append("test");
-        });
+        .below_of(&output_widget, 32);
+    let serial_port_list = SerialPort::available_ports();
+
+    assert!(serial_port_list.is_ok());
+
+    serial_port_list.unwrap().iter().for_each(move |item| {
+        let choice_to_add = item.to_str();
+        if choice_to_add.is_some() {
+            serial_port_selector.add_choice(choice_to_add.unwrap());
+        }
+    });
 
     wind.make_resizable(true);
     wind.end();
