cc0d205a4c5983afa4fb3f132ea7098630586b06
Fabien ROUSSEL <fabien.roussel.1992@gmail.com>
2022-09-29 22:44:05 +0200

    Opens the right serial port when selected in the Choice menu.

--- serial_term/src/main.rs
+++ serial_term/src/main.rs
@@ -6,8 +6,31 @@ use fltk::{
     prelude::*,
     window::Window,
 };
-use serial2::SerialPort;
+use serialport;
+use std::rc::Rc;
+
+struct SerialAppCore {
+    serial_port: Option<Box<dyn serialport::SerialPort>>,
+}
+
+impl SerialAppCore {
+    pub fn open_port(&mut self, port_name: &str, baud_rate: u32) {
+        // TODO assert baud rate is standard baud rate.
+        // assert port_name not empty.
+        let serial_port_builder = serialport::new(port_name, baud_rate);
+        let serial_port_err_code = serial_port_builder.open();
+        if serial_port_err_code.is_ok() {
+            self.serial_port = Some(serial_port_err_code.unwrap());
+        }
+    }
+}
+
 fn main() {
+    // backend:
+    let serial_port_list = serialport::available_ports();
+    let mut core = Rc::<SerialAppCore>::new(SerialAppCore { serial_port: None });
+
+    // GUI
     let app = app::App::default();
     let mut wind = Window::default()
         .with_size(400, 200)
@@ -17,24 +40,27 @@ fn main() {
     let mut serial_port_selector = Choice::default()
         .with_size(100, 20)
         .below_of(&output_widget, 32);
-    let serial_port_list = SerialPort::available_ports();
 
     assert!(serial_port_list.is_ok());
 
     serial_port_list.unwrap().iter().for_each(move |item| {
-        let choice_to_add = item.to_str();
-        if choice_to_add.is_some() {
-            serial_port_selector.add(
-                choice_to_add.unwrap(),
-                Shortcut::None,
-                MenuFlag::Normal,
-                |choice| {
-                    let serial_port_to_open = choice.choice();
-                    assert!(serial_port_to_open.is_some());
-                    let _serial_port = SerialPort::open(serial_port_to_open.unwrap(), 115200);
-                },
-            );
-        }
+        let choice_to_add = &item.port_name;
+        let mut core_clone = Rc::clone(&mut core);
+        serial_port_selector.add(
+            choice_to_add.as_str(),
+            Shortcut::None,
+            MenuFlag::Normal,
+            move |choice| {
+                let serial_port_to_open = choice.choice();
+                assert!(serial_port_to_open.is_some());
+                let mut_core = Rc::get_mut(&mut core_clone);
+                if mut_core.is_some() {
+                    mut_core
+                        .unwrap()
+                        .open_port(serial_port_to_open.unwrap().as_str(), 9600);
+                }
+            },
+        );
     });
 
     wind.make_resizable(true);
